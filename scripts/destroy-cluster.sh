51fc6f5c23c9aad70815ec381298974538d5ec0f
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-03-26 14:07:19 -0400

    added service account cleanup

--- scripts/destroy-cluster.sh
+++ scripts/destroy-cluster.sh
@@ -15,6 +15,12 @@ kubectl delete clusterrolebinding service-pod-reader-gateway
 kubectl delete clusterrolebinding service-pod-reader-organization
 kubectl delete clusterrolebinding service-pod-reader-mongo
 
+kubectl delete serviceaccount $SA_NAME -n $NAMESPACE_DEPARTMENT
+kubectl delete serviceaccount $SA_NAME -n $NAMESPACE_EMPLOYEE
+kubectl delete serviceaccount $SA_NAME -n $NAMESPACE_GATEWAY
+kubectl delete serviceaccount $SA_NAME -n $NAMESPACE_ORGANIZATION
+kubectl delete serviceaccount $SA_NAME -n $NAMESPACE_MONGO
+
 kubectl delete clusterrole microservices-kubernetes-namespace-reader
 
 kubectl delete namespace $NAMESPACE_DEPARTMENT
