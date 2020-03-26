ccad68ae6b22faf1883eff1985c822c6dbb047ee
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-03-26 10:06:31 -0400

    removed build project with maven - it must be done within Docker image

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -17,33 +17,22 @@ docker images
 mvn clean
 
 cd department-service
-mvn clean package install
 docker build -t vmware/department:1.1 .
 cd ..
 
 cd gateway-service
-mvn clean package install
 docker build -t vmware/gateway:1.1 .
 cd ..
 
 cd organization-service
-mvn clean package install
 docker build -t vmware/organization:1.1 .
 cd ..
 
 
 cd employee-service
-mvn clean package install
 docker build -t vmware/employee:1.1 .
 cd ..
 
-#minikube profile $CLUSTER2_NAME
-#eval $(minikube docker-env)
-#docker images
-
-#mvn clean
 docker images
 
 cd scripts
-
-# minikube delete --all
