ce344019634116f4f174f386b92c8842fcd0f5ca
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-03-26 14:06:08 -0400

    added service account name veriable: SA_NAME

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -8,4 +8,6 @@ export NAMESPACE_GATEWAY=gateway
 export NAMESPACE_ORGANIZATION=organization
 export NAMESPACE_MONGO=mongo
 
+export SA_NAME=api-service-account
+
 export CLUSTER1_NAME=minikube-cluster-1
